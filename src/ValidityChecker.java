ea322471dbad07fe479a0e98d2176a8434e36ff0
mvlvth <mvlvth@gmail.com>
2021-03-09 23:30:09 +0100

    Algorithm works!

--- src/ValidityChecker.java
+++ src/ValidityChecker.java
@@ -45,31 +45,38 @@ public class ValidityChecker {
         int sumOfANumber = 0;
         int checkSum = 0;
 
-        for (int i = 2; i < persNo.length; i++) {
+        for (int i = 2; i < persNo.length - 1; i++) {
             /*
              * if(i%2 == 0){ tempNo = persNo[i]*2; } else{ tempNo = persNo[i]*1; }
              */
+            System.out.println("TESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT: " + i);
 
             tempNo = (i % 2 == 0) ? persNo[i] * 2 : persNo[i] * 1;
 
-            char[] charArray = String.valueOf(tempNo).toCharArray();
-            if (charArray.length > 1) {
+            char[] charArr = String.valueOf(tempNo).toCharArray();
+            if (charArr.length > 1) {
                 // int[] twoDigits = new int[charArray.length];
-                for (int j = 0; j < charArray.length; j++) {
+                for (int j = 0; j < charArr.length; j++) {
                     System.out.println("TEST 1: " + sumOfANumber);
 
                     // twoDigits[j] = Integer.parseInt(String.valueOf(charArray[j]));
-                    sumOfANumber += Integer.parseInt(String.valueOf(charArray[j]));
+                    sumOfANumber += Integer.parseInt(String.valueOf(charArr[j]));
                     System.out.println("TEST: " + sumOfANumber);
-                    System.out.println(sumOfANumber);
-                    sumOfPersNo += sumOfANumber;
+                    // System.out.println(sumOfANumber);
                 }
+                sumOfPersNo += sumOfANumber;
+
+                System.out.println("IF");
+                // reset
+                sumOfANumber = 0;
 
             } else {
                 sumOfPersNo += tempNo;
-                System.out.println("summan av enstaka siffror: " + sumOfPersNo);
-
+                // System.out.println("summan av enstaka siffror: " + sumOfPersNo);
+                System.out.println("ELSE");
             }
+            System.out.println("VARJE SIFFERSTEG: " + sumOfPersNo);
+
         }
 
         checkSum = (10 - (sumOfPersNo % 10)) % 10;
